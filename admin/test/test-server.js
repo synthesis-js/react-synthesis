7d64f6b7a4e51583f95e1b86a69542bb97e3fe1f
Mike McCready <mikem@inspero.net>
2016-11-21 14:52:21 -0800

    add tests for user POST

--- admin/test/test-server.js
+++ admin/test/test-server.js
@@ -18,7 +18,23 @@ describe('Users', () => {
 	  	});
 	});
 	it('should list a SINGLE user on /api/user/<id> GET');
-	it('should add a SINGLE user on /api/user POST');
+	it('should add a SINGLE user on /api/user POST', (done) => {
+	  chai.request(app)
+	    .post('/api/user')
+	    .send({'email': 'testing@email.com', 'password': 'testing', 'name': 'Test Guy'})
+	    .end(function(err, res){
+	      res.should.have.status(200);
+	      res.should.be.json;
+	      res.body.should.be.a('object');
+	      res.body.should.have.property('name');
+	      res.body.should.have.property('email');
+	      res.body.should.have.property('_id');
+	      res.body.should.have.property('password');
+	      res.body.should.have.property('created_at');
+	      res.body.should.have.property('user_type');
+	      done();
+	    });
+	});
 	it('should update a SINGLE user on /api/user/<id> PUT');
 	it('should delete a SINGLE user on /api/user/<id> DELETE');
 });
